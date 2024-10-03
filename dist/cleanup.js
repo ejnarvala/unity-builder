b40043bc923095fdb49a08dc6d4278df73bcf5ee
Ejnar Arechavala <ejnar.arechavala@onepeloton.com>
2024-10-03 14:25:51 -0700

    remove recursive list

--- dist/cleanup.js
+++ dist/cleanup.js
@@ -1,23 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-async function listFilesRecursively(dir) {
-  try {
-    const files = await fs.promises.readdir(dir, { withFileTypes: true });
-    for (const file of files) {
-      const fullPath = path.join(dir, file.name);
-      if (file.isDirectory()) {
-        console.log(`Directory: ${fullPath}`);
-        await listFilesRecursively(fullPath); // Recursively list files in the directory
-      } else {
-        console.log(`File: ${fullPath}`);
-      }
-    }
-  } catch (err) {
-    console.error(`Error reading directory ${dir}:`, err);
-  }
-}
-
 async function listFiles() {
   try {
     const files = await fs.promises.readdir(__dirname);
@@ -47,16 +30,6 @@ async function listFiles() {
   } catch (err) {
     console.error('Error reading workspace files:', err);
   }
-
-
-  try {
-    console.log('\nList files in GITHUB_WORKSPACE:');
-    const workspace = process.env.GITHUB_WORKSPACE;
-    await listFilesRecursively(workspace);
-  } catch (err) {
-    console.error('Error reading GITHUB_WORKSPACE recursively:', err);
-  }
-
 }
 
 
