5d5558319a9442d5aee8ff9ca35ad9bea2f1de98
Ejnar Arechavala <ejnar.arechavala@onepeloton.com>
2024-10-03 12:01:02 -0700

    update cleanup

--- dist/cleanup.js
+++ dist/cleanup.js
@@ -1,9 +1,35 @@
-const { Input } = require('./model');
+const fs = require('fs');
+const path = require('path');
 
-async function runCleanup() {
-  console.log("Hello from cleanup.js");
-  let floatingLicenseId = Input.getInput('FLOATING_LICENSE');
-  console.log(`Floating License ID: ${floatingLicenseId}`);
+async function listFiles() {
+  try {
+    const files = await fs.promises.readdir(__dirname);
+    console.log('Files in current directory:');
+    files.forEach(file => {
+      console.log(file);
+    });
+  } catch (err) {
+    console.error('Error reading directory:', err);
+  }
+
+  try {
+    console.log('\nEnvironment Variables:');
+    for (const [key, value] of Object.entries(process.env)) {
+      console.log(`${key}: ${value}`);
+    }
+  } catch (err) {
+    console.error('Error reading environment variables:', err);
+  }
+
+  try {
+    const files = await fs.promises.readdir("/github/workspace");
+    console.log('\nFiles in /github/workspace:');
+    files.forEach(file => {
+      console.log(file);
+    });
+  } catch (err) {
+    console.error('Error reading github/workspace:', err);
+  }
 }
 
-runCleanup();
+listFiles();
